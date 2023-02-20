8259a8f08147e17e4079197a9c7794bd8a0b366f
Rishabh Singh <ris8645208@gmail.com>
2023-02-20 22:41:15 +0530

    fix: update location access before login

--- app/context/DataContext/index.js
+++ app/context/DataContext/index.js
@@ -140,16 +140,17 @@ const DataContextProvider = ({ children }) => {
 
   useEffect(() => {
     (async () => {
+      if (isAuth) {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
 
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        setTimeout(() => setLocation(null))
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setTimeout(() => setLocation(null))
     })();
   }, []);
 
