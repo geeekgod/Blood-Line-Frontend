7d165202275c5ce29d8bd7449b30092040055cf3
Rishabh Singh <ris8645208@gmail.com>
2024-02-24 23:16:54 +0530

    fix: trial 1

--- app/screens/Login/index.js
+++ app/screens/Login/index.js
@@ -1,59 +1,90 @@
-import * as React from 'react';
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
-import { StyleSheet, View, Platform } from 'react-native';
-import { heightScreen, widthScreen } from '../../utils/layout';
-import { Box, Button, CloseIcon, HStack, Icon, IconButton, Text, VStack } from 'native-base';
+import * as React from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { StyleSheet, View, Platform } from "react-native";
+import { heightScreen, widthScreen } from "../../utils/layout";
+import {
+  Box,
+  Button,
+  CloseIcon,
+  HStack,
+  Icon,
+  IconButton,
+  Text,
+  VStack,
+} from "native-base";
 import LoginUnlock from "../../assets/svg/login/LoginUnlock";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 import { Alert } from "native-base";
-import bloodLineApi from '../../api';
-import { AuthContext } from '../../context/AuthContext';
-import { useNavigation } from '@react-navigation/native';
+import bloodLineApi from "../../api";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigation } from "@react-navigation/native";
 import { makeRedirectUri } from "expo-auth-session";
-
+import Constants from "expo-constants";
 
 WebBrowser.maybeCompleteAuthSession();
+const EXPO_REDIRECT_PARAMS = {
+  useProxy: true,
+  projectNameForProxy: "@swade-io/swade",
+};
+// const NATIVE_REDIRECT_PARAMS = { native: "com.geeekgod.bloodline.auth://" };
+const NATIVE_REDIRECT_PARAMS = { native: "com.geeekgod.bloodline://" };
+const REDIRECT_PARAMS =
+  Constants.appOwnership === "expo"
+    ? EXPO_REDIRECT_PARAMS
+    : NATIVE_REDIRECT_PARAMS;
+const redirectUri = makeRedirectUri(REDIRECT_PARAMS);
+console.log(
+  `redirectUri: ${redirectUri}, Constants.appOwnership: ${Constants.appOwnership}`
+);
 
 const Login = () => {
-
   const [showError, setShowError] = React.useState(false);
-  const { storeCredentials } = React.useContext(AuthContext)
-  const navigation = useNavigation()
+  const { storeCredentials } = React.useContext(AuthContext);
+  const navigation = useNavigation();
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: '938415613346-3ecn64agj1971r8l0s520kc41d4s0n6p.apps.googleusercontent.com',
-    androidClientId: '938415613346-f70dmihvkcjpjo3ljm798e85t5g45s2i.apps.googleusercontent.com',
-    iosClientId: "938415613346-fcq85l5n5bgi4j8porff4aqj991b6kcd.apps.googleusercontent.com",
-    redirectUri: Platform.select({
-      ios: undefined,
-      android: makeRedirectUri({
-        native: `com.geeekgod.bloodline.auth://`,
-        useProxy: true,
-      }),
-    }),
+    expoClientId:
+      "938415613346-3ecn64agj1971r8l0s520kc41d4s0n6p.apps.googleusercontent.com",
+    androidClientId:
+      "938415613346-f70dmihvkcjpjo3ljm798e85t5g45s2i.apps.googleusercontent.com",
+    iosClientId:
+      "938415613346-fcq85l5n5bgi4j8porff4aqj991b6kcd.apps.googleusercontent.com",
+    redirectUri: redirectUri,
+    // redirectUri: Platform.select({
+    //   ios: makeRedirectUri({
+    //     native: `com.geeekgod.bloodline://`,
+    //     useProxy: true,
+    //   }),
+    //   android: makeRedirectUri({
+    //     native: `com.geeekgod.bloodline.auth://`,
+    //     useProxy: true,
+    //   }),
+    // }),
   });
 
   React.useEffect(() => {
-    if (response?.type === 'success') {
+    if (response?.type === "success") {
       const { authentication } = response;
 
-      bloodLineApi.post("/auth/googleAuth", {
-        accessToken: authentication.accessToken
-      }).then((res) => {
-        storeCredentials(res.data);
-        if(res.data.isProfile){
-          setTimeout(() => navigation.navigate("HomeNavigation"), 200)
-        }
-        else{
-          setTimeout(() => navigation.navigate("CreateProfile"), 200)
-        }
-      }).catch((err) => {
-        console.log(err);
-        setShowError(true)
+      bloodLineApi
+        .post("/auth/googleAuth", {
+          accessToken: authentication.accessToken,
+        })
+        .then((res) => {
+          storeCredentials(res.data);
+          if (res.data.isProfile) {
+            setTimeout(() => navigation.navigate("HomeNavigation"), 200);
+          } else {
+            setTimeout(() => navigation.navigate("CreateProfile"), 200);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setShowError(true);
 
-        setTimeout(() => setShowError(false), 5000);
-      })
+          setTimeout(() => setShowError(false), 5000);
+        });
     }
   }, [response]);
 
@@ -66,46 +97,76 @@ const Login = () => {
         top={heightScreen > 800 ? "-20" : "-40"}
         right={heightScreen > 800 ? "-120" : "-150"}
       >
-        <AntDesign name='heart' size={400} color='#DE2A26' />
+        <AntDesign name="heart" size={400} color="#DE2A26" />
       </Box>
       <LoginUnlock />
-      <Text fontFamily='body' fontWeight="600" mt='6' fontSize='3xl' textAlign='center'>Continue with us</Text>
-      <Text fontFamily='body' color='#7A7A7A' mt='3' mb='3' fontWeight="400" fontSize='md' textAlign='center' lineHeight='2xl'>Sign in via</Text>
+      <Text
+        fontFamily="body"
+        fontWeight="600"
+        mt="6"
+        fontSize="3xl"
+        textAlign="center"
+      >
+        Continue with us
+      </Text>
+      <Text
+        fontFamily="body"
+        color="#7A7A7A"
+        mt="3"
+        mb="3"
+        fontWeight="400"
+        fontSize="md"
+        textAlign="center"
+        lineHeight="2xl"
+      >
+        Sign in via
+      </Text>
       <Button
         leftIcon={<Icon name="google" as={AntDesign} color="white" />}
         disabled={!request}
         onPress={() => promptAsync()}
-        size='lg'
-        fontSize='xl'
-        rounded='2xl'
+        size="lg"
+        fontSize="xl"
+        rounded="2xl"
       >
         Login with Google
       </Button>
 
-      {showError && (<Alert w="100%" status='error' position='absolute' bottom='8'>
-        <VStack space={2} flexShrink={1} w="100%">
-          <HStack flexShrink={1} space={2} alignItems='center' justifyContent="space-between">
-            <HStack space={2} flexShrink={1}>
-              <Alert.Icon mt="1" />
-              <Text fontSize="md" color="coolGray.800">
-                There was some error
-              </Text>
+      {showError && (
+        <Alert w="100%" status="error" position="absolute" bottom="8">
+          <VStack space={2} flexShrink={1} w="100%">
+            <HStack
+              flexShrink={1}
+              space={2}
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <HStack space={2} flexShrink={1}>
+                <Alert.Icon mt="1" />
+                <Text fontSize="md" color="coolGray.800">
+                  There was some error
+                </Text>
+              </HStack>
+              <IconButton
+                onPress={() => setShowError(false)}
+                variant="unstyled"
+                _focus={{
+                  borderWidth: 0,
+                }}
+                icon={<CloseIcon size="3" />}
+                _icon={{
+                  color: "coolGray.600",
+                }}
+              />
             </HStack>
-            <IconButton onPress={() => setShowError(false)} variant="unstyled" _focus={{
-              borderWidth: 0
-            }} icon={<CloseIcon size="3" />} _icon={{
-              color: "coolGray.600"
-            }} />
-          </HStack>
-        </VStack>
-      </Alert>)}
-
-
+          </VStack>
+        </Alert>
+      )}
     </View>
   );
-}
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
